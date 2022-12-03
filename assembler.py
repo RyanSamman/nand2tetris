13f066d231a342ba713acef6a9213145c3ddc981
RyanSamman <RyanSamman@outlook.com>
2022-12-03 10:06:55 +0300

    Cleanup Assembler Part 1

--- assembler.py
+++ assembler.py
@@ -1,10 +1,12 @@
 import re
-from typing import List, Set, Tuple
+from typing import List, Tuple
+
 
 def isLabel(s: str) -> bool:
-    return not (re.match(r"^\(.+\)$",s) is None)
+    return not (re.match(r"^\(.+\)$", s) is None)
 
-def buildLabelMap(symbolList: List[str]) -> dict[str,int]:
+
+def buildLabelMap(symbolList: List[str]) -> dict[str, int]:
     labelMap: dict[str, int] = {
         "SP": 0,
         "LCL": 1,
@@ -23,12 +25,12 @@ def buildLabelMap(symbolList: List[str]) -> dict[str,int]:
         "R7": 7,
         "R8": 8,
         "R9": 9,
-        "R10":10,
-        "R11":11,
-        "R12":12,
-        "R13":13,
-        "R14":14,
-        "R15":15,
+        "R10": 10,
+        "R11": 11,
+        "R12": 12,
+        "R13": 13,
+        "R14": 14,
+        "R15": 15,
     }
 
     address = 0
@@ -37,7 +39,7 @@ def buildLabelMap(symbolList: List[str]) -> dict[str,int]:
         if isLabel(s):
             labelMap[s[1:-1]] = address
         else:
-            address+= 1
+            address += 1
 
     address = 16
     for s in symbolList:
@@ -46,9 +48,10 @@ def buildLabelMap(symbolList: List[str]) -> dict[str,int]:
 
         if s[0] == "@" and not s[1:].isnumeric() and not s[1:] in labelMap:
             labelMap[s[1:]] = address
-            address += 1 
+            address += 1
 
     return labelMap
+
 
 def removeInvalidLines(symbolList: List[str]) -> List[str]:
     lst: List[str] = []
@@ -56,12 +59,13 @@ def removeInvalidLines(symbolList: List[str]) -> List[str]:
         x = s.split("//")
         s = x[0].strip()
 
-        if (len(s) == 0):
+        if len(s) == 0:
             continue
 
         lst.append(s)
 
-    return lst 
+    return lst
+
 
 def convertStrToSymbols(symbolList: List[str]) -> List[str]:
     symbolList = removeInvalidLines(symbolList)
@@ -69,7 +73,7 @@ def convertStrToSymbols(symbolList: List[str]) -> List[str]:
     commandList: List[str] = list(filter(lambda s: not isLabel(s), symbolList))
 
     # Replace Labels and convert to binary
-    for (i,s) in enumerate(commandList):
+    for (i, s) in enumerate(commandList):
         if s[0] == "@":
             # If label, get it from the labelMap, and update
             if not s[1:].isnumeric():
@@ -81,12 +85,13 @@ def convertStrToSymbols(symbolList: List[str]) -> List[str]:
 
                 s = f"@{address}"
 
-            commandList[i] = f"{int(s[1:]):b}".zfill(16) 
+            commandList[i] = f"{int(s[1:]):b}".zfill(16)
 
         else:
             commandList[i] = cInstrToBinary(s)
 
     return commandList
+
 
 def splitCInstToDestCompJump(cInst: str) -> Tuple[str, str, str]:
     destStr: str = ""
@@ -99,7 +104,7 @@ def splitCInstToDestCompJump(cInst: str) -> Tuple[str, str, str]:
         compStr = x[0]
 
     else:
-        destStr = x[0] 
+        destStr = x[0]
         compStr = x[1]
 
     y = compStr.split(";")
@@ -111,8 +116,9 @@ def splitCInstToDestCompJump(cInst: str) -> Tuple[str, str, str]:
         jumpStr = y[1]
 
     return (destStr, compStr, jumpStr)
-    
-def processA(s:str) -> str:
+
+
+def processA(s: str) -> str:
     return "1" if s.find("M") != -1 else "0"
 
 
@@ -147,62 +153,71 @@ compMap = {
     "D|M": "010101",
 }
 
-def processComp(s:str) -> str:
+
+def processComp(s: str) -> str:
 
     return compMap[s]
 
+
 jumpMap = {
-    "null":"000",
+    "null": "000",
     "JGT": "001",
     "JEQ": "010",
     "JGE": "011",
     "JLT": "100",
     "JNE": "101",
     "JLE": "110",
-    "JMP":"111"
+    "JMP": "111",
 }
 
-def processJump(s:str) -> str:
+
+def processJump(s: str) -> str:
     if len(s) == 0:
         s = "null"
     return jumpMap[s]
 
-def processDest(s:str) -> str:
+
+def processDest(s: str) -> str:
     dest = ""
     dest += "1" if s.find("A") != -1 else "0"
     dest += "1" if s.find("D") != -1 else "0"
     dest += "1" if s.find("M") != -1 else "0"
 
-    return dest 
+    return dest
+
 
 def cInstrToBinary(cInst: str) -> str:
-    (destStr, compStr, jumpStr) = splitCInstToDestCompJump(cInst);
+    (destStr, compStr, jumpStr) = splitCInstToDestCompJump(cInst)
     return f"111{processA(compStr)}{processComp(compStr)}{processDest(destStr)}{processJump(jumpStr)}"
+
 
 def saveSymbolsToFile(outFile: str, opList: list[str]) -> None:
     with open(outFile, "w") as file:
-        file.writelines(opList);
-        
+        file.writelines(opList)
+
+
 def loadLinesFromFile(inFile: str) -> List[str]:
     with open(inFile, "r") as file:
         return [line.strip() for line in file]
 
 
-def main(inputFileName: str, outputFileName:str):
+def main(inputFileName: str, outputFileName: str):
     with open(inputFileName, "r") as inputFile, open(outputFileName, "w") as outputFile:
         symbols = convertStrToSymbols(list(inputFile))
         outputFile.write("\n".join(symbols))
 
+
 if __name__ == "__main__":
+    path = "projects/06"
     files = [
-        "./add/Add",
-        "./max/Max",
-        "./max/MaxL",
-        "./pong/Pong",
-        "./pong/PongL",
-        "./rect/Rect",
-        "./rect/RectL",
+        "/add/Add",
+        "/max/Max",
+        "/max/MaxL",
+        "/pong/Pong",
+        "/pong/PongL",
+        "/rect/Rect",
+        "/rect/RectL",
     ]
 
     for f in files:
-        main(f"{f}.asm", f"{f}.hack")
+        main(f"{path}{f}.asm", f"{path}{f}.hack")
